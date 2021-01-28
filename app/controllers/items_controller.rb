30eca64a46a49e6d785bba0ac2a227f24af7266d
combatcellist <combatcellist913@gmail.com>
2021-01-28 15:32:03 +0900

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,46 @@
+class ItemsController < ApplicationController
+
+  before_action :find_item, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path, notice: "メニューが投稿されました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path, notice: "メニューが更新されました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :price, :content).merge(user_id: current_user.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+end
